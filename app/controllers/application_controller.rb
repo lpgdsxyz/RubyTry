952ce3347c31dc31dcfc54169454a2937f24149f
lipei <1036129755@qq.com>
2011-07-29 15:46:05 +0800

    access control completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+	before_filter :authorize
+	
   protect_from_forgery
   
   private
@@ -11,5 +13,11 @@ class ApplicationController < ActionController::Base
 		end
 		session[:cart_id] = cart.id
 		cart
+	end
+  protected
+	def authorize
+		unless User.find_by_id(session[:user_id])
+			redirect_to login_url, :notice => "Please Log In"
+		end
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,18 @@ class User < ActiveRecord::Base
 	
 	validate	:password_must_be_present
 	
-	def encrypt_password(password,salt)
-		Digest::SHA2.hexdigest(password + "wibble" + salt)
-	end
 	
 	class << self
-		def anthenticate(name, password)
+		def authenticate(name, password)
 			if user = find_by_name(name)
-				if user.hashed_password==encrypt_password(password,user.salt)
+				if user.hashed_password==user.class.encrypt_password(password,user.salt)
 					user
 				end
 			end
+		end
+		
+		def encrypt_password(password,salt)
+			Digest::SHA2.hexdigest(password + "wibble" + salt)
 		end
 	end
 	
@@ -29,7 +30,7 @@ class User < ActiveRecord::Base
 		@password=password
 		if password.present?
 			generate_salt
-			self.hashed_password =self.encrypt_password(password,salt)
+			self.hashed_password =self.class.encrypt_password(password,salt)
 		end
 	end
 	
