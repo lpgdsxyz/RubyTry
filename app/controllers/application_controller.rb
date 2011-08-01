ffaf8b3899d92688309a03dd744f475a5dc61113
lipei <1036129755@qq.com>
2011-08-01 16:33:44 +0800

    internationalized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,27 @@
 class ApplicationController < ActionController::Base
 	before_filter :authorize
 	
-  protect_from_forgery
+	before_filter :set_i18n_locale_from_params
+	
+	protect_from_forgery
+  
+	protected 
+  
+	def set_i18n_locale_from_params
+		if params[:locale]
+			if I18n.available_locales.include?(params[:locale].to_sym)
+				I18n.locale = params[:locale]
+			else
+				flash.now[:notice] = 
+					"#{params[:locale]} translation not available"
+				logger.error flash.now[:notice]
+			end
+		end
+	end
+  
+	def default_url_options
+		{:locale => I18n.locale}
+	end
   
   private
 	def current_cart
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Depot::Application.routes.draw do
 
-#	get "admin/index"
-#	get "sessions/new"
-#	get "sessions/create"
-#	get "sessions/destroy"
-#	get "store/index"
-
 	get "admin" => "admin#index"
 	
 	controller :sessions do
@@ -13,20 +7,19 @@ Depot::Application.routes.draw do
 		post "login" => :create
 		delete "logout" => :destroy
 	end
-	
-	
-#	scope '(:locale)' do
+		
+	scope '(:locale)' do
 		resources :users
 		resources :orders
 		resources :line_items
-		resources :carts
+		resources :carts 
 		
 		resources :products do
-		
-		get :who_bought, :on =>:member
-			get :who_bought, :on => :member
+			get :who_bought, :on =>:member
 		end
-
+		
+		root :to => "store#index", :as => 'store'
+	end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -77,7 +70,7 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  root :to => "store#index", :as => 'store'
+  
 
   # See how all your routes lay out with "rake routes"
 
@@ -85,5 +78,5 @@ Depot::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
   
-#  end
+
 end
